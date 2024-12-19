e00187aa9416cbb5243808ec606770e92e86ccc4
Tobiasz Mazurek <Tobiasz2817@outlook.com>
2024-12-19 02:09:38 +0100

    clean up/assemblify params extend

--- Assemblify.cs
+++ Assemblify.cs
@@ -59,9 +59,16 @@ namespace CoreUtility {
     [System.Serializable]
     public class AssemblyDefinitionData {
         public string name;
+        public string rootNamespace;
         public string[] references;
-        public string[] optionalUnityReferences;
-        public string[] versionDefines;
+        public string[] includePlatforms;
+        public string[] excludePlatforms;
         public bool allowUnsafeCode;
+        public bool overrideReferences;
+        public string[] precompiledReferences;
+        public bool autoReferenced;
+        public string[] defineConstraints;
+        public string[] versionDefines;
+        public bool noEngineReferences;
     }
 }
